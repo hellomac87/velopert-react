7525d864dea538bd8640e3f48f5fb9b3fb6b92d3
hellomac87 <hellomac87@gmail.com>
2018-07-18 15:28:25 +0900

    배열 다루기 (1) 생성과 렌더링

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import PhoneForm from './components/PhoneForm';
+import PhoneInfoList from './components/PhoneInfoList';
 
 class App extends Component {
+  id = 2
+  state = {
+    information :[
+      {
+        id : 0,
+        name : 'Master Thief',
+        phone : '111-1111-1111'
+      },
+      {
+        id : 1,
+        name : 'AI',
+        phone : '222-2222-2222'
+      }
+    ]
+  }
 
   handleCreate = (data) => {
-    console.log(data);
+    const {information} = this.state;
+    this.setState({
+      information : information.concat({
+        id : this.id++,
+        ...data
+      })
+    })
   }
 
   render() {
+    const {information} = this.state;
     return (
       <div>
         <PhoneForm
           onCreate={this.handleCreate}
         />
+        {/* {JSON.stringify(information)} */}
+        <PhoneInfoList data={this.state.information}/>
       </div>
     );
   }
